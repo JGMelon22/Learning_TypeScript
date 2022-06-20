f1a991af9bb55bb104233939875285fd5ace89f9
JGMelon22 <jgvasconcellos22@gmail.com>
2022-06-20 13:50:06 -0300

    The Any type

---
+++ src/CoreTypes/Any/app.js
@@ -0,0 +1,8 @@
+// Any
+// Avoid as much as possible
+// Otherwise, will be some sort of vanilla JS
+var favoriteActivities;
+favoriteActivities = ["Coding", 69];
+favoriteActivities.forEach(function (element) {
+    console.log(element);
+});
---
+++ src/CoreTypes/Any/app.ts
@@ -0,0 +1,9 @@
+// Any
+// Avoid as much as possible
+// Otherwise, will be some sort of vanilla JS
+let favoriteActivities: any[];
+favoriteActivities = ["Coding", 69]
+
+favoriteActivities.forEach(element => {
+    console.log(element);
+});

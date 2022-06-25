f6b3b53eae26681a9d3a0e1e2d3b0914ef55d7f5
JGMelon22 <jgvasconcellos22@gmail.com>
2022-06-25 20:14:42 -0300

    TODO - List Exercise p1

---
+++ HandsOn/TODO-List/app.js
@@ -0,0 +1,8 @@
+// TODO - List
+// Using in memory items
+var list = document.querySelector("#list");
+var form = document.querySelector("#list");
+var input = document.querySelector("#new-task-tile");
+input === null || input === void 0 ? void 0 : input.addEventListener("click", function () {
+    window.alert("Hello from click!");
+});
---
+++ HandsOn/TODO-List/app.ts
@@ -0,0 +1,12 @@
+// TODO - List
+// Using in memory items
+
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.querySelector<HTMLFormElement>("#list");
+const input = document.querySelector<HTMLInputElement>("#new-task-tile");
+
+/*
+input?.addEventListener("click", function () {
+    window.alert("Hello from click!")
+});
+*/  

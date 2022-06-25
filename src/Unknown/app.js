4aaecd612bc6752311e1718f6d254f4c3e41300c
JGMelon22 <jgvasconcellos22@gmail.com>
2022-06-25 18:17:27 -0300

    Unknown Type

--- src/Unknown/app.js
+++ src/Unknown/app.js
@@ -1,2 +1,25 @@
+"use strict";
+exports.__esModule = true;
 // Unknown Type
-console.log("Hello from TS");
+var clc = require("cli-color");
+var stuff;
+var stringBuilderMockUp = [];
+function strMock() {
+    stringBuilderMockUp.push("".concat(clc.green.bold("Working with \"Unknown Type\"\n")), "".concat(clc.white.bold("stuff = true"), " - ").concat(typeof (stuff = true), "\n"), "".concat(clc.white.bold("stuff = \"Hello, There\""), " - ").concat(typeof (stuff = "Hello, There"), "\n"), "".concat(clc.white.bold("stuff = []"), " - ").concat(typeof (stuff = []), "\n"), "".concat(clc.white.bold("stuff = {}"), " - ").concat(typeof (stuff = {}), "\n"), "".concat(clc.white.bold("stuff = Math.random"), " - ").concat(typeof (stuff = Math.random), "\n"), "".concat(clc.white.bold("stuff = null"), " - ").concat(typeof (stuff = null), "\n"), "".concat(clc.white.bold("stuff = undefined"), " - ").concat(typeof (stuff = undefined), "\n"), "".concat(clc.white.bold("stuff = new TypeError()"), "; - ").concat(typeof (stuff = new TypeError())));
+    return stringBuilderMockUp.join("");
+}
+console.log(strMock());
+// Another eg
+var userInput;
+var userName;
+userInput = console.log("\n".concat(clc.blue.bold("User Input:"), " ").concat(userInput = "Nice"));
+/* The type 'unknown' is not assignable to type 'string
+* userName = userInput; // Error
+*/
+if (typeof (userInput) === "string") {
+    console.log("".concat(clc.blue.bold("userName = userInput"), ": ").concat(userName = userInput));
+}
+// A better safe than sorry check with unknown type
+else {
+    console.log(userName = clc.red.bold("The type 'unknown' is not assignable to type 'string"));
+}
--- src/Unknown/app.ts
+++ src/Unknown/app.ts
@@ -1,2 +1,41 @@
 // Unknown Type
-console.log("Hello from TS");
+import * as clc from "cli-color";
+
+var stuff: unknown;
+var stringBuilderMockUp: string[] = [];
+
+function strMock(): string[] | string {
+    stringBuilderMockUp.push(
+        `${clc.green.bold("Working with \"Unknown Type\"\n")}`,
+        `${clc.white.bold("stuff = true")} - ${typeof (stuff = true)}\n`,
+        `${clc.white.bold("stuff = \"Hello, There\"")} - ${typeof (stuff = "Hello, There")}\n`,
+        `${clc.white.bold("stuff = []")} - ${typeof (stuff = [])}\n`,
+        `${clc.white.bold("stuff = {}")} - ${typeof (stuff = {})}\n`,
+        `${clc.white.bold("stuff = Math.random")} - ${typeof (stuff = Math.random)}\n`,
+        `${clc.white.bold("stuff = null")} - ${typeof (stuff = null)}\n`,
+        `${clc.white.bold("stuff = undefined")} - ${typeof (stuff = undefined)}\n`,
+        `${clc.white.bold("stuff = new TypeError()")}; - ${typeof (stuff = new TypeError())}`
+    );
+
+    return stringBuilderMockUp.join("");
+}
+
+console.log(strMock());
+
+// Another eg
+let userInput: unknown;
+let userName: string;
+
+userInput = console.log(`\n${clc.blue.bold("User Input:")} ${userInput = "Nice"}`);
+
+/* The type 'unknown' is not assignable to type 'string
+* userName = userInput; // Error
+*/
+if (typeof (userInput) === "string") {
+    console.log(`${clc.blue.bold("userName = userInput")}: ${userName = userInput}`);
+}
+
+// A better safe than sorry check with unknown type
+else {
+    console.log(userName = clc.red.bold("The type 'unknown' is not assignable to type 'string"));
+}
